3971b918c51c7191c334900f708e38b710f78e86
enea5 <eneaa.aliko@gmail.com>
2021-06-27 08:19:08 -0700

    v1.0.1

--- mycustom.php
+++ mycustom.php
@@ -3,7 +3,7 @@
 	Plugin Name: My Custom Plugin
 	Plugin URI: https://alex-next.netlify.app
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.0
+	Version: 1.0.1
 	Author: Alex
 	Author URI: https://alex-next.netlify.app
 */
@@ -13,6 +13,7 @@ if( ! class_exists( 'Auto_Updater' ) ) {
 $updater = new Auto_Updater( __FILE__ );
 $updater->set_username( 'enea5' );
 $updater->set_repository( 'mycustom' );
+/** Here is test for updating again */
 /*
 	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
 */
